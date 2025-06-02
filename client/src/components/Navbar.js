c082c4681661dd46b26365495e6e1ee691eb504b
Jonathan MacDuff <jonathan.macduff@outlook.com>
2025-06-01 20:24:52 -0700

    Added basic Navbar component framework

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h2>My Website</h2>
+      <ul>
+        <li>Home</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
