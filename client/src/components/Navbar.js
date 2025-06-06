bcbb205ee0cabcac1b2b578fa80fb27b50d498df
Jonathan MacDuff <jonathan.macduff@outlook.com>
2025-06-05 20:13:19 -0700

    added routing

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
       <h2>My Website</h2>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/projects'>Projects</Link></li>
       </ul>
     </nav>
   );
