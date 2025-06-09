5316e2efb3a1819c1042638a6125a5190ab9c8d4
Jonathan MacDuff <jonathan.macduff@outlook.com>
2025-06-08 19:53:59 -0700

    Added brief descriptions of projects

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -6,9 +6,11 @@ const Projects = () => {
         <h1>My Projects</h1>
         <div>
           <h2>Cat Meme Generator</h2>
+          <p>This application is designed to generate random cat memes in three varieties utilizing public APIs.</p>
         </div>
         <div>
           <h2>Pet Finder</h2>
+          <p>This application is designed to allow users to interact with eachother via  messages and comments to find and report lost pets.</p>
         </div>
     </div>
   );
