f89ed2e0f926b5709f49aacd4619710bf4c96b5c
Jonathan MacDuff <jonathan.macduff@outlook.com>
2025-06-09 21:44:16 -0700

    added brief about me

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,6 +4,8 @@ const About = () => {
   return (
     <div>
         <h1>About Me</h1>
+        <p>Hi! I've been a top-performing salesperson for most of my life, in spite of how ill-suited that field is to my personality. 
+        I'm looking to make a name for myself in the tech space now, bringing the same commitment and drive to an area that actually suits me.</p>
     </div>
   );
 };
