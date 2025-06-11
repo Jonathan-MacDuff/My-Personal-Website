118380db6a1f09c88d1b1e4b934ca6986eff3c5c
Jonathan MacDuff <jonathan.macduff@outlook.com>
2025-06-11 14:57:05 -0700

    edited about me and added basic formatting for h1 and p elements

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div>
         <h1>About Me</h1>
         <p>Hi! I've been a top-performing salesperson for most of my life, in spite of how ill-suited that field is to my personality. 
-        I'm looking to make a name for myself in the tech space now, bringing the same commitment and drive to an area that actually suits me.</p>
+        I'm looking to make a name for myself in the tech space now, bringing the same commitment and drive to an area that comes much more naturally.</p>
     </div>
   );
 };
