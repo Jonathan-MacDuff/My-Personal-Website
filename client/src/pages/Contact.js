575049a89ebd84a370d3c0d958bda0271de09520
Jonathan MacDuff <jonathan.macduff@outlook.com>
2025-06-13 19:44:11 -0700

    added inpute types to contact form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -6,9 +6,9 @@ function Contact() {
         <div>
             <h1>Contact Me</h1>
             <form>
-                <input placeholder='Name'/>
-                <input placeholder='Phone'/>
-                <input placeholder='Email'/>
+                <input type='name' placeholder='Name'/>
+                <input type='tel' placeholder='Phone'/>
+                <input type='email' placeholder='Email'/>
                 <textarea placeholder='Message'/>
                 {/* add checkboxes for prefered contact method */}
                 <button type='submit'>Submit</button>
