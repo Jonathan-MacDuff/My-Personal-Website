8e83fe3cc28a2842d47f4583acd256e589167df1
Jonathan MacDuff <jonathan.macduff@outlook.com>
2025-07-04 20:58:50 -0700

    added github links to projects

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -6,10 +6,12 @@ function Projects() {
         <h1>My Projects</h1>
         <div>
           <h2>Cat Meme Generator</h2>
+          <a href='https://github.com/Jonathan-MacDuff/Cat-Meme-Generator'>Github Link</a>
           <p>This application is designed to generate random cat memes in three varieties utilizing public APIs.</p>
         </div>
         <div>
           <h2>Pet Finder</h2>
+          <a href='https://github.com/Jonathan-MacDuff/Pet-Finder-V2'>Github Link</a>
           <p>This application is designed to allow users to interact with eachother via  messages and comments to find and report lost pets.</p>
         </div>
     </div>
