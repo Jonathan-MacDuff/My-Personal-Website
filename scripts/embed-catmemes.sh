7cdd54d629718ad747a73e6e2f6037306b2325d9
Jonathan MacDuff <jonathan.macduff@outlook.com>
2025-08-08 22:30:33 -0700

    new catmeme embed script

--- scripts/embed-catmemes.sh
+++ scripts/embed-catmemes.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-echo "Copying Cat Meme Generator to website..."
-rm -rf ../client/public/Cat-Meme-Generator/*
-cp -r ../Cat-Meme-Generator/* ../client/public/Cat-Meme-Generator/
-
-echo "Cat Meme Generator embedded successfully!"
-
-
-: <<'END_COMMENT'
-For later React deplyment:
-
-#!/bin/bash
-
-# Build Cat Meme Generator React app
 cd ../Cat-Meme-Generator/client || exit
 PUBLIC_URL=/Cat-Meme-Generator npm run build
 
-# Copy build into website's public folder
 rm -rf ../../client/public/Cat-Meme-Generator/*
 cp -r build/* ../../client/public/Cat-Meme-Generator/
 
 echo "Cat Meme Generator React app embedded successfully!"
-END_COMMENT
+
+echo "📦 Syncing Cat Meme Generator backend..."
+
+SRC_BACKEND_DIR="../server"
+DEST_BACKEND_DIR="../../server/catmemes"
+
+mkdir -p "$DEST_BACKEND_DIR"
+
+rsync -av --exclude='__pycache__' --exclude='*.pyc' --exclude='*.pyo' "$SRC_BACKEND_DIR/" "$DEST_BACKEND_DIR/"
+
+echo "Backend synced to $DEST_BACKEND_DIR"
+
+echo "Cat Meme Generator fully embedded!"
