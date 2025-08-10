90b6d1171eb8306878130ca846551e0018c58f3d
Jonathan MacDuff <jonathan.macduff@outlook.com>
2025-08-09 18:39:24 -0700

    second attempt to fix catmemes CORS issue

--- server/catmemes/app.py
+++ server/catmemes/app.py
@@ -4,7 +4,7 @@ import json
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "https://autistic-insight.com"}})
 
 def load_quotes(file_path):
     with open(file_path, 'r') as f:
