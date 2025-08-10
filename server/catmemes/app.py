e83b061e830155107224ba947382b500b9d6a3ef
Jonathan MacDuff <jonathan.macduff@outlook.com>
2025-08-09 19:33:47 -0700

    fourth attempt to fix catmemes CORS issue correction

--- server/catmemes/app.py
+++ server/catmemes/app.py
@@ -8,22 +8,22 @@ def load_quotes(file_path):
     with open(file_path, 'r') as f:
         return json.load(f)
     
-@catmemes_bp.route.route('/api/cat')
+@catmemes_bp.route('/api/cat')
 def get_cat():
     cats = load_quotes('data/cats.json')
     return jsonify({'cat': random.choice(cats)})
 
-@catmemes_bp.route.route('/api/affirmation')
+@catmemes_bp.route('/api/affirmation')
 def get_affirmation():
     affirmations = load_quotes('data/affirmations.json')
     return jsonify({'affirmation': random.choice(affirmations)})
 
-@catmemes_bp.route.route('/api/insult')
+@catmemes_bp.route('/api/insult')
 def get_insult():
     insults = load_quotes('data/insults.json')
     return jsonify({'insult': random.choice(insults)})
 
-@catmemes_bp.route.route('/api/badass')
+@catmemes_bp.route('/api/badass')
 def get_badass():
     quotes = load_quotes('data/bbquotes.json')
     return jsonify({'quote': random.choice(quotes)})
